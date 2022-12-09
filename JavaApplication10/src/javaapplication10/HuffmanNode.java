353480cbb6d7bc8ccbc1528fd72f238f6b5ccac6
Dogukan444 <102189710+Dogukan444@users.noreply.github.com>
2022-12-09 16:02:48 +0300

    Dizi ile Huffman

---
+++ JavaApplication10/src/javaapplication10/HuffmanNode.java
@@ -0,0 +1,17 @@
+
+package javaapplication10;
+// Huffman Coding in Java
+
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+class HuffmanNode {
+    
+  int item;
+  char c;
+  HuffmanNode left;
+  HuffmanNode right;
+
+}
+    
+
